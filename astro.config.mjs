d4e4663b0e26abf78248a8d031ae60ff685a49cb
Valentín Lorente <vlorentejimenez@gmail.com>
2024-03-11 03:49:23 +0100

    Add Vercel Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "hybrid",
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
+});
